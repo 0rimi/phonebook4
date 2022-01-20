bc43a85c7e8cf517b82d10ab056b2948e9bfcaab
이영림 <rm1113@naver.com>
2022-01-20 11:07:24 +0900

    DataSource

--- src/main/java/com/javaex/dao/PhoneDao.java
+++ src/main/java/com/javaex/dao/PhoneDao.java
@@ -8,6 +8,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.javaex.vo.PersonVo;
@@ -15,27 +18,19 @@ import com.javaex.vo.PersonVo;
 @Repository //저장소의 개념으로 쓸거니까 (applicationContext참고)
 public class PhoneDao {
 	
+	@Autowired
+	private DataSource dataSource;
+	
 	// 0. import java.sql.*;
-	Connection conn = null;
-	PreparedStatement pstmt = null;
-	ResultSet rs = null;
-
-	private String driver = "oracle.jdbc.driver.OracleDriver";
-	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
-	private String id = "phonedb";
-	private String pw = "phonedb";
-
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+	
+	//연결
 	private void getConnection() {
 		try {
-			// 1. JDBC 드라이버 (Oracle) 로딩
-			Class.forName(driver);
-
-			// 2. Connection 얻어오기
-			conn = DriverManager.getConnection(url, id, pw);
-			// System.out.println("접속성공");
-
-		} catch (ClassNotFoundException e) {
-			System.out.println("error: 드라이버 로딩 실패 - " + e);
+			// DataSource에서 커넥션 받아오기
+			conn = dataSource.getConnection();
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		}
